5edca731d74edc3f10e2f2f3248bfcce534a8eef
CEIBAfernan.roman <fernan.roman@ceiba.com.co>
2021-06-27 21:18:36 -0500

    Se finaliza desarrollo tanto en frontend como en backend

---
+++ src/components/cityBikes.js
@@ -0,0 +1,60 @@
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { MapBikes } from "./mapBikes";
+import { SocketContext } from "../context/socket";
+import { useStations } from "../hooks/useStations";
+export const CityBikes = () => {
+  const socket = useContext(SocketContext);
+  const endpoint = "http://127.0.0.1:4001";
+  const zoom = 13;
+  const cityBikeInformationEndpoint = `${endpoint}/cityBikeInformation`;
+  const [information, setInformation] = useState({
+    lat: 51.505,
+    lng: -0.09,
+    locationName: "",
+    cityName: "",
+  });
+  const [stations, setStations] = useState([]);
+  const [startSocket, setStartSocket] = useState(false);
+  const { addStations } = useStations();
+
+  useEffect(() => {
+    getCityBikeInformation();
+  }, []);
+
+  const getCityBikeInformation = async function () {
+    const {
+      data: { location, name, stations },
+    } = await axios.get(cityBikeInformationEndpoint);
+    setInformation({
+      lat: location.latitude,
+      lng: location.longitude,
+      locationName: location.city,
+      cityName: name,
+    });
+    setStations(stations);
+    setStartSocket(true);
+  };
+
+  useEffect(() => {
+    if (startSocket && stations) {
+      socket.on("city-bike-stations", (stations) => {
+        setStations(stations);
+        addStations(stations);
+      });
+      return () => socket.off("city-bike-stations");
+    }
+  }, [startSocket]);
+
+  const position = [information.lat, information.lng];
+  return (
+    <div className="map">
+      {information.cityName !== "" && (
+        <h1 className="mb-4">
+          {information.cityName} - {information.locationName}
+        </h1>
+      )}
+      <MapBikes position={position} stations={stations} zoom={zoom} />
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { SocketContext, socket } from "./context/socket";
+ReactDOM.render(
+  <SocketContext.Provider value={socket}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </SocketContext.Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
